a42b2f4eae59829c310e2b2beed4fbf6a7a0f152
Aarron Hulswitt <Ahulswitt@trgrepair.local>
2020-01-14 15:58:14 -0500

    Created the properties and types that we get back from USGS.  Updated the HelloWorld.vue component to use the new types and make a call to USGS api to get the values for a single (hard coded) river

--- src/data/usgsData.ts
+++ src/data/usgsData.ts
@@ -1,3 +1,37 @@
 export interface USGSData{
-    
+    value: USGSValue
+}
+
+export interface USGSValue{
+    timeSeries: Array<TimeSeries>
+}
+
+export interface TimeSeries{
+    sourceInfo: SourceInfo,
+    variable: Variable,
+    values: Values,
+    name: string
+}
+
+export interface SourceInfo{
+    siteName: string    
+}
+
+export interface Variable{
+    variableName: string,
+    variableDescription: string,
+    unit: Unit
+}
+
+export interface Values{
+    values: Array<Value>
+}
+
+export interface Unit{
+    unitCode: string
+}
+
+export interface Value{
+    value: string,
+    dateTime: Date
 }
