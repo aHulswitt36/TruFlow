e0676d43b4bca15a282a6657ab328b0d5adf30ee
Aarron Hulswitt <aarron.hulswitt@gmail.com>
2020-01-14 22:58:31 -0500

    Created apiClient service to be a wrapper of axios to make calls to apis. updated HelloWorld component to properly call apiClient to get river data.  Next we will have to set up HelloWorld to be a async component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,31 @@
 <template>
   <div class="hello">
-    <!-- <span>{{riverData.value.timeSeries[0].sourceInfo.siteName}}</span>
+    <span>{{riverData.value.timeSeries[0].sourceInfo.siteName}}</span>
     <ul>
       <li :v-for="gaugeData in riverData.value.timeSeries">
         <h2>{{ gaugeData.variable.variableName }}</h2>
         <h4>{{ gaugeData.variable.variableDescription }}</h4>
         <h4>Unit: {{gaugeData.variable.unit.unitCode}}</h4>
       </li>
-    </ul> -->
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
 import {ServerResponse} from '../data/ServerResponse';
-import axios from 'axios';
+import { getById } from '../services/apiClient';
 import { USGSData } from '../data/usgsData';
 
 @Component
 export default class HelloWorld extends Vue {
   @Prop() private msg!: string;
-  name = "RiverData";
-  data(){
-    return{
-      riverData: {} as USGSData
-    }    
-  }
-  mounted(){
-    axios.request<USGSData>({
-      url: 'https://waterservices.usgs.gov/nwis/iv/?format=json&sites=04201500&period=PT4H&siteStatus=all',
-      transformResponse: (r: ServerResponse) => r.data
-    }).then((response) => {
-      console.log(response.data);
-      this.$data.riverData = response.data;
-    })
-  }
+  name = "RiverData"; 
+  riverData!: USGSData;
 
+  async created(){
+    this.riverData = await getById('04201500');
+  }
 }
 </script>
 
---
+++ src/services/apiClient.ts
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+const http = axios.create({
+    headers: { 'Content-Type': 'application/json' }
+});
+
+export async function getById(riverId: string){
+    const response = await http.get(`https://waterservices.usgs.gov/nwis/iv/?format=json&period=PT4H&siteStatus=all&sites=${riverId}`);
+    return response.data;
+}
