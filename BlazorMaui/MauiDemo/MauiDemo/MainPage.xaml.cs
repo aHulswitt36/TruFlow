5ac1d0ff98549ad1b2a0407e8378f9aca3c995bd
Aarron Hulswitt <ahulswitt@embracepetinsurance.com>
2021-07-06 21:44:56 -0400

    Added updated and working projects for Maui Blazor and Maui standard. deleted the two other projects and removed all the bin/obj folders

---
+++ BlazorMaui/MauiDemo/MauiDemo/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using MauiDemo.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Maui.Controls;
+using System;
+
+namespace MauiDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly IServiceProvider _serviceProvider;
+        public MainPage(IServiceProvider serviceProvider)
+        {
+            InitializeComponent();
+            _serviceProvider = serviceProvider;
+        }
+
+        int count = 0;
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+            CounterLabel.Text = $"Current count: {count}";
+        }
+
+
+        async void OnRiversClicked(object sender, EventArgs e)
+        {
+            var vm = _serviceProvider.GetRequiredService<RiversViewModel>();
+            await (App.Current.MainPage as NavigationPage).PushAsync(new Rivers(vm));
+        }
+    }
+}
---
+++ BlazorMaui/MauiDemo/MauiDemo/Startup.cs
@@ -0,0 +1,53 @@
+﻿using Infrastructure;
+using MauiDemo.Pages;
+using MauiDemo.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Hosting;
+using Microsoft.Maui.LifecycleEvents;
+using Newtonsoft.Json;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace MauiDemo
+{
+    public class Startup : IStartup
+    {
+        public void Configure(IAppHostBuilder appBuilder)
+        {
+            appBuilder
+                .UseMauiApp<App>()
+                .UseMicrosoftExtensionsServiceProviderFactory()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    var assembly = Assembly.GetExecutingAssembly();
+                    var file = assembly.GetManifestResourceStream(
+                        assembly.GetManifestResourceNames().FirstOrDefault(r => r.EndsWith("settings.json", System.StringComparison.OrdinalIgnoreCase))
+                    );
+                    using var sr = new StreamReader(file);
+                    var json = sr.ReadToEnd();
+                    var settings = JsonConvert.DeserializeObject<Infrastructure.Settings.BaseSettings>(json);
+                    services.AddSingleton(settings.UsgsSettings);
+                    services.AddComponentLibrary("https://waterservices.usgs.gov/nwis/");
+                    services.AddTransient<RiversViewModel>();
+                    services.AddTransient(serviceType: typeof(Page),
+                        implementationType: typeof(CustomNavigationPage));
+                }).ConfigureLifecycleEvents(lifecycle => {
+#if ANDROID
+					lifecycle.AddAndroid(d => {
+						d.OnBackPressed(activity => {
+							System.Diagnostics.Debug.WriteLine("Back button pressed!");
+						});
+					});
+#endif
+                });
+        }
+    }
+}
